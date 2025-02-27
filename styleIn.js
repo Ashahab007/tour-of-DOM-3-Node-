e143210c3c0c78f574a96000d1d78f77d2108794
Ammar Shahab <ammar.qa@generalpharma.com>
2025-02-27 14:40:30 +0600

    added some concept

--- styleIn.js
+++ styleIn.js
@@ -1,12 +1,14 @@
+// 1. childNodes, firstChild, nextSibling, previousSibling
+
 const placesContainer = document.getElementById("places-container");
 console.log(placesContainer.childNodes); //NodeList(5) [text, h3, text, ul, text]
 console.log(placesContainer.firstChild); //#text
 console.log(placesContainer.childNodes[3]); //<ul></ul>
 console.log(placesContainer.childNodes[3].childNodes); //NodeList(9) [text, li, text, li, text, li, text, li, text]
 
-console.log(placesContainer.childNodes[3].childNodes[2].nextSibling); //Kuakata
+console.log(placesContainer.childNodes[3].childNodes[2].nextSibling.innerText); //Kuakata
 
-// createElement:
+// 2. createElement:
 const foodsUl = document.querySelector("#food-container ul");
 console.log(foodsUl);
 
@@ -20,7 +22,8 @@ const li2 = document.createElement("li");
 li2.innerText = "Sheekh Kabab";
 foodsUl.appendChild(li2);
 
+//3. parentNode
 // finding parent node of foodsUl
-console.log(foodsUl.parentNode);
-console.log(foodsUl.parentNode.parentNode);
-console.log(foodsUl.parentNode.parentNode.parentNode);
+console.log(foodsUl.parentNode); //section#food-container
+console.log(foodsUl.parentNode.parentNode); //main
+console.log(foodsUl.parentNode.parentNode.parentNode); //body
